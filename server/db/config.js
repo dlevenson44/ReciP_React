62b2f374c0327aa3d234682a63d98a252ba021b5
dlevenson44 <daniellevenson4@gmail.com>
2018-05-08 17:46:28 -0400

    setup server/db/config file

---
+++ server/db/config.js
@@ -0,0 +1,29 @@
+// set options object
+const options = {
+	query: (e) => {
+		// console log each query
+		console.log(e.query)
+	}
+}
+
+// import pg-promise, assign to variable with options
+const pgp = require('pg-promise')(options)
+
+// add conditional database for dev and test
+function setDatabase() {
+	if(process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+		return pgp({
+			database: 'recipes_db',
+			port: 5432,
+			host: 'localhost',
+		})
+	} else if(process.env.NODE_ENV === 'production') {
+		return pgp(process.env.DATABASE_URL)
+	}
+}
+
+// call pgp conditional function
+const db = setDatabase();
+
+// export
+module.exports = db

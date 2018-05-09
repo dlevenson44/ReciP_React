84caedf810244bf37bbe1f0670038096023cba91
dlevenson44 <daniellevenson4@gmail.com>
2018-05-09 17:43:51 -0400

    added confirm password function

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Register extends Component {
     constructor() {
+        super()
         this.state = {
             username: '',
             password: '',
@@ -10,6 +11,7 @@ class Register extends Component {
             name: ''
         }
         this.handleInputChange = this.handleInputChange.bind(this)
+        this.passwordConfirm = this.passwordConfirm.bind(this)
     }
 
     handleInputChange(e) {
@@ -20,6 +22,16 @@ class Register extends Component {
         })
     }
 
+    passwordConfirm() {
+        if (this.state.password !== this.state.confirmPassword) {
+            return (
+                <div>
+                    <p>Passwords do not match.</p>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
@@ -27,6 +39,7 @@ class Register extends Component {
                     <input type="text" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
                     <input type="password" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
                     <input type="password" name="confirmPassword" value={this.state.confirmPassword} placeholder="Confirm Password" onChange={this.handleInputChange} />
+                    {this.passwordConfirm()}
                     <input type="email" name="email" value={this.state.email} placeholder="Email" onChange={this.handleInputChange} />
                     <input type="text" name="name" value={this.state.name} placeholder="Name" onChange={this.handleInputChange} />
                     <input type="submit" value="Create Account" />
@@ -35,3 +48,5 @@ class Register extends Component {
         )
     }
 }
+
+export default Register;

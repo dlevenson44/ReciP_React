39dac184130086141fe56115b97b2b94dbc8202b
dlevenson44 <daniellevenson4@gmail.com>
2018-05-09 15:55:04 -0400

    added routes to top level component

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,30 @@ class App extends Component {
     return (
       <div className="App">
         <Header header="ReciP" />
+
+        <Route exact path="/login" render={() => (
+          this.state.auth
+          ? <Redirect to="/favorites" />
+          : <Login handleLoginSubmit={this.handleLoginSubmit} />
+        )} />
+
+        <Route exact path="/register" render={() => (
+          this.state.auth
+          ? <Redirect to="/favorites" />
+          : <Register handleRegisterSubmit={this.handleRegisterSubmit} />
+        )} />
+
+        <Route exact path="/favorites" render={() => (
+          this.state.auth
+          ? <Favorites user={this.state.user} />
+          : <Error />
+        )} />
+
+        <Route exact path="/profile" render={() => (
+          this.state.auth
+          ? <Profile user={this.state.user} handleUpdateSubmit={this.handleUpdateSubmit} userDelete={this.userDelete} />
+          : <Error />
+        )} />
       </div>
     );
   }

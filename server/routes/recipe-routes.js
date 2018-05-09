945464f7ea0fad36126cf99dea545ec19dfca92b
dlevenson44 <daniellevenson4@gmail.com>
2018-05-09 18:55:36 -0400

    fixed recipe routes

--- server/routes/recipe-routes.js
+++ server/routes/recipe-routes.js
@@ -1,27 +1,15 @@
 // import dependencies
 const express = require('express')
-const recipesRoutes = express.Router()
+const recipesRouter = express.Router()
 const recipeHelpers = require('../services/recipes/recipe-helpers')
 const recipesController = require('../controllers/recipes-controller')
-const usersController = require('../controllers/users-controller')
 
-// sett add route
-recipesRoutes.get('/add', (req, res) => {
-	res.render('favorite_recipes', {
-		auth: (req.user) ? true : false,
-	})
-})
-
-// set API fetch route
-recipesRoutes.get('/:search', recipeHelpers.getRecipes, recipesController.sendApiRecipe)
-
-// set create and show favorite routes
-recipesRoutes.post('/', recipesController.create)
-recipesRoutes.get('/:id', recipesController.show)
-recipesRoutes.post('/favorite_recipes/:id', recipesController.create)
-
-// set delete recipe
-recipesRoutes.delete('/:id', recipesController.delete)
-
-// export routes
-module.exports = recipesRoutes
+module.exports = function(app) {
+	app.route('/')
+		.post(recipesController.create)
+		.get(recipeHelpers.getRecipes, recipesController.sendApiRecipe)
+	
+	app.route('/favorites')
+		.get(recipesController.index)
+		.delete(recipesController.delete)
+	};

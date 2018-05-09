64c61122ccb2fc34c60bb9be169e8e8ed2add80a
dlevenson44 <daniellevenson4@gmail.com>
2018-05-09 16:44:51 -0400

    set up error component

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Uh Oh!</h1>
+            <h5>You shouldn't be here!</h5>
+            <p>Something went wrong!  Please click on the back button to return to the previous screen.</p>
+        </div>
+    )
+}
+
+export default Error;

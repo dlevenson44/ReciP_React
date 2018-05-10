9f07f473cadeaa4835178903df673a3cc7c9f7ed
dlevenson44 <daniellevenson4@gmail.com>
2018-05-10 14:52:37 -0400

    changed recipe from functional to class component

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import IngredientList from './IngredientList';
 
-const Recipe = (props) => {
-    const calPerServing = (props.recipe.calories / props.recipe.yield).toPrecision(6)
-    console.log(props.recipe)
-    return (
-        <div>
-            <h3>{props.recipe.label} -- <a href={props.recipe.url}>Full Recipe</a></h3>
-            <p>{props.recipe.dietLabels[0]}</p>
-            <div className="recipe-container">
+class Recipe extends Component {
+    render () {
+        const calPerServing = (this.props.recipe.calories / this.props.recipe.yield).toPrecision(6)
+        console.log(this.props.recipe)
+        return (
             <div>
-            <img src={props.recipe.image} />
-            <p>{props.recipe.dietLabels[0]}</p>
-            <p>{calPerServing} calories per serving</p>            
-            <p>{props.recipe.yield} servings</p>
+                <h3>{this.props.recipe.label} -- <a href={this.props.recipe.url}>Full Recipe</a></h3>
+                <p>{this.props.recipe.dietLabels[0]}</p>
+                <div className="recipe-container">
+                <div>
+                <img src={this.props.recipe.image} />
+                <p>{this.props.recipe.dietLabels[0]}</p>
+                <p>{calPerServing} calories per serving</p>            
+                <p>{this.props.recipe.yield} servings</p>
+                </div>
+                <div>
+                <IngredientList ingredients={this.props.recipe.ingredients} />
+                </div>
+                </div>
             </div>
-            <div>
-            <IngredientList ingredients={props.recipe.ingredients} />
-            </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Recipe;

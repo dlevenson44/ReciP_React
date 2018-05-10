0895c52a40141d9874978f418821615102e0a68f
dlevenson44 <daniellevenson4@gmail.com>
2018-05-10 11:51:27 -0400

    created and rendering ingredientlist component

---
+++ client/src/components/IngredientList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IngredientList = (props) => {
+    return (
+        <div>
+            <p>ingredeitns</p>
+        </div>
+    )
+}
+
+export default IngredientList;

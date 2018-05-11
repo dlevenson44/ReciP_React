125b2e149d9826e92c5752d4c7d8961bc1e4b154
dlevenson44 <daniellevenson4@gmail.com>
2018-05-11 12:46:47 -0400

    put fetch favorites in lifecycle, fixed endless loop

--- client/src/components/FavoritesList.jsx
+++ client/src/components/FavoritesList.jsx
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 
 class FavoritesList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            favorites: {}
+        }
+        this.fetchFavorites = this.fetchFavorites.bind(this)
+    }
+
+    componentWillMount() {
+        this.fetchFavorites()
+    }
+
+    // fetch favorite recipes
+    fetchFavorites() {
+        console.log('fetched favorite')
+        fetch('/api/recipes/favorites', {
+            method: 'GET',
+            credentials: 'include'
+        }).then(res => res.json())
+        .then(res => {
+            this.setState({
+                favorites: res
+            })
+        })
+    }
+
     render() {
+        // this.fetchFavorites()
         return (
             <div>
                 <p>FavoritesList Component</p>

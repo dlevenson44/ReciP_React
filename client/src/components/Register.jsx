24c48bf3138d6033b4fc06c77c1030ae786b413f
dlevenson44 <daniellevenson4@gmail.com>
2018-05-14 13:30:26 -0400

    rendering message for taken credentials

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -30,18 +30,23 @@ class Register extends Component {
     }
 
     compareCredentials(e) {
+        let counter = 0;
         for (let i = 0; i < this.state.fetchedResults.length; i++) {
             console.log(this.state.fetchedResults[i])
             if (this.state.fetchedResults[i].username === this.state.username) {
                 console.log('username taken')
-                return <p>Username taken</p>
+                let counter = counter + 1;
+                return (<p>Username taken</p>)
             } else if (this.state.fetchedResults[i].email === this.state.email) {
                 console.log('email taken')
-                return <p>There is already an account associated with this email</p>
+                let counter = counter + 1;
+                return (<p>There is already an account associated with this email</p>)
             }
         }
-        console.log('creating account')
-        this.props.handleRegisterSubmit(e, this.state)
+        if (counter > 0) {
+            console.log('creating account')
+            this.props.handleRegisterSubmit(e, this.state)
+        }
     }
     
 
@@ -69,11 +74,12 @@ class Register extends Component {
                 <form onSubmit={(e) => this.searchTakenCredentials(e)}>
                     <input type="text" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
                     <input type="password" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
-                    <input type="password" name="confirmPassword" value={this.state.confirmPassword} placeholder="Confirm Password" onChange={this.handleInputChange} />
-                    {this.passwordConfirm()}
+                    <input type="password" name="confirmPassword" value={this.state.confirmPassword} placeholder="Confirm Password" onChange={this.handleInputChange} />                    
                     <input type="email" name="email" value={this.state.email} placeholder="Email" onChange={this.handleInputChange} />
                     <input type="submit" value="Create Account" />
-                </form>
+                </form> 
+                {this.passwordConfirm()}
+                {this.compareCredentials()}               
             </div>
         )
     }

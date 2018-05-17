763f9996cc6340e6e60d5f77a53a0861aa5316a7
dlevenson44 <daniellevenson4@gmail.com>
2018-05-17 17:26:13 -0400

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.use((req, res, next) => {
 	console.log('---', req.user, req.path);
@@ -47,6 +47,11 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 });
+
+app.use(express.static(path.join(__dirname, '../../client/build')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
 // routes
 const authRoutes = require('./routes/auth-routes')
